554dd8ba33808bba35a6f9cc2533343eb26d9b22
aleksandr_radchenko <aleksandr_radchenko@epam.com>
2017-11-15 18:27:47 +0300

    Lambda.part2.exercise1 (Radchenko) Fix getFullName and equality of ints

--- src/test/java/lambda/part2/exercise/Exercise1.java
+++ src/test/java/lambda/part2/exercise/Exercise1.java
@@ -33,7 +33,7 @@ public class Exercise1 {
 
         // TODO создать переменную sameAgesChecker: (Person, Person) -> boolean, используя BiPredicate
 
-        BiPredicate<Person, Person> sameAgesChecker = (p1, p2) -> Objects.equals(p1.getAge(), p2.getAge());
+        BiPredicate<Person, Person> sameAgesChecker = (p1, p2) -> p1.getAge() == p2.getAge();
 
          assertTrue(sameAgesChecker.test(person1, person2));
          assertFalse(sameAgesChecker.test(person1, person3));
@@ -44,20 +44,19 @@ public class Exercise1 {
     }
 
     // TODO метод getFullName: Person -> String, извлекающий из объекта Person строку в формате "имя фамилия".
-     private static Function<Person, String> getFullName(Person p) {
-        return p.getFirstName() + " " + p.getLastName();
+     private static Function<Person, String> getFullName() {
+        return p -> p.getFirstName() + " " + p.getLastName();
     }
 
     // TODO метод createExtractorAgeOfPersonWithTheLongestFullName: (Person -> String) -> ((Person, Person) -> int),
     // TODO - принимающий способ извлечения полного имени из объекта Person
     // TODO - возвращающий BiFunction, сравнивающий два объекта Person и возвращающий возраст того, чье полное имя длиннее.
-     private static BiFunction<Person, Person, Integer> createExtractorAgeOfPersonWithTheLongestFullName(Function<Person, String> propExtractor) {
+     private static BiFunction<Person, Person, Integer> createExtractorAgeOfPersonWithTheLongestFullName(
+             final Function<Person, String> fullNameExtractor) {
         return (p1, p2) -> {
-            int p1NameLength = propExtractor.apply(p1).length();
-            int p2NameLength = propExtractor.apply(p2).length();
-            if (p1NameLength > p2NameLength) return p1.getAge();
-            else return p2.getAge();
-
+            final int p1NameLength = fullNameExtractor.apply(p1).length();
+            final int p2NameLength = fullNameExtractor.apply(p2).length();
+            return p1NameLength > p2NameLength? p1.getAge(): p2.getAge();
         };
      }
 
@@ -67,7 +66,7 @@ public class Exercise1 {
         Person person2 = new Person("Илья", "Жирков", 22);
 
         // TODO воспользоваться ссылкой на метод getFullName
-        Function<Person, String> getFullName = Exercise1::getFullName;
+        Function<Person, String> getFullName = getFullName();
 
         // (Person, Person) -> Integer
         // TODO воспользоваться методом createExtractorAgeOfPersonWithTheLongestFullName
