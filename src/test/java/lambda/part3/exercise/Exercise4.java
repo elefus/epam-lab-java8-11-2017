4a7feb4e6048ecafe052ef7f4be70d11c644e336
ferventdev <alex.4everyours@gmail.com>
2017-11-19 18:15:08 +0300

    Part 3. Exercise 4. Imports optimized (Shevkunenko)

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -7,7 +7,6 @@ import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
