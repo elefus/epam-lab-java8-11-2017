96b0574770124e6693c8e2818890536f7b26115b
Mihail Poplavkov <mihail.poplavkov@gmail.com>
2017-11-20 10:52:56 +0300

    Lambda part 3 exercise 1

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -1,6 +1,7 @@
 package lambda.part3.exercise;
 
 import lambda.data.Employee;
+import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
@@ -13,15 +14,27 @@ import static org.junit.Assert.assertEquals;
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class Exercise1 {
 
+    private String fullNameExtractor(Employee employee) {
+        Person person = employee.getPerson();
+        return String.format("%s %s", person.getFirstName(), person.getLastName());
+    }
+
+    private Integer stringLengthExtractor(String s) {
+        return s.length();
+    }
+
+    private Integer fullNameLengthExtractor(Employee employee) {
+        return stringLengthExtractor(fullNameExtractor(employee));
+    }
+
     @Test
     public void mapEmployeesToLengthOfTheirFullNames() {
         List<Employee> employees = Example1.getEmployees();
-        List<Integer> lengths = null;
+        List<Integer> lengths = new ArrayList<>(employees.size());
 
-        // TODO функция извлечения полного имени из сотрудника fullNameExtractor: Employee -> String
-        // TODO функция извлечения длины из строки stringLengthExtractor: String -> Integer
-        // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
-        // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
+        for(Employee employee: employees) {
+            lengths.add(fullNameLengthExtractor(employee));
+        }
 
         assertEquals(Arrays.asList(14, 19, 14, 14), lengths);
     }
