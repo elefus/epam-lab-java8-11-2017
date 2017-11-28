6a256b2490e04dbd587010b88cd4aec8a7e3b390
aleksandr_zheludkov <aleksandr_zheludkov@epam.com>
2017-11-28 18:07:03 +0300

    Spliterator forkjoiningpool is done.

--- src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
+++ src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
@@ -3,6 +3,7 @@ package spliterator.part1.exercise.forkjoinpool;
 import org.junit.Test;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
@@ -15,8 +16,42 @@ public class FJPSumIntArrayExample {
                               .limit(100)
                               .toArray();
 
-        // TODO реализация
-        int result = new ForkJoinPool().invoke(null);
+
+        int result = new ForkJoinPool().invoke(new FJSumArrayTask(data, 0, 99));
         assertEquals(100, result);
+    }
+
+    private static class FJSumArrayTask extends RecursiveTask<Integer> {
+        private static final int SEQUENTIAL_THRESHOLD = 30;
+        int[] data;
+        int from;
+        int to;
+
+        public FJSumArrayTask(int[] data, int from, int to){
+            this.data = data;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        protected Integer compute() {
+            if ((to - from) < SEQUENTIAL_THRESHOLD){
+                int result = 0;
+                for (int i = from; i < to + 1; i++) {
+                    result += data[i];
+                }
+                return result;
+
+            }
+            else {
+                int partition = (to + from) / 2;
+                FJSumArrayTask left = new FJSumArrayTask(data, from, partition);
+                left.fork();
+
+                FJSumArrayTask right = new FJSumArrayTask(data, partition + 1, to);
+                return right.compute() + left.join();
+            }
+
+        }
     }
 }
