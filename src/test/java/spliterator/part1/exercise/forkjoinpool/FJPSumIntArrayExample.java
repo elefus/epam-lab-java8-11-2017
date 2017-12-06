e449be74d04a7d051279c895a6884795b1021e96
super64812 <iserdz812@gmail.com>
2017-12-06 20:20:16 +0300

    spliterator.part1.forkJoinPool

--- src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
+++ src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
@@ -2,7 +2,9 @@ package spliterator.part1.exercise.forkjoinpool;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
@@ -16,7 +18,39 @@ public class FJPSumIntArrayExample {
                               .toArray();
 
         // TODO реализация
-        int result = new ForkJoinPool().invoke(null);
+        int result = new ForkJoinPool().invoke(new ForkJoinPoolSumArrayTask(data, 0, data.length - 1));
         assertEquals(100, result);
+    }
+
+    private static class ForkJoinPoolSumArrayTask extends RecursiveTask<Integer>{
+
+        private static final int SEQUENTIAL_THRESHOLD = 10;
+
+        private int[] data;
+        private int fromInclusive;
+        private int toInclusive;
+
+        public ForkJoinPoolSumArrayTask(int[] data, int fromInclusive, int toInclusive){
+            this.data = data;
+            this.fromInclusive = fromInclusive;
+            this.toInclusive = toInclusive;
+        }
+
+        @Override
+        protected Integer compute() {
+            if(toInclusive - fromInclusive < SEQUENTIAL_THRESHOLD){
+
+                return Arrays.stream(data).skip(fromInclusive).limit(toInclusive - fromInclusive + 1).sum();
+
+            } else {
+
+                int pivot = (fromInclusive + toInclusive)/2;
+                FJPSumIntArrayExample.ForkJoinPoolSumArrayTask left = new FJPSumIntArrayExample.ForkJoinPoolSumArrayTask(data, fromInclusive, pivot);
+                left.fork();
+
+                FJPSumIntArrayExample.ForkJoinPoolSumArrayTask right = new FJPSumIntArrayExample.ForkJoinPoolSumArrayTask(data, pivot + 1, toInclusive);
+                return left.join() + right.compute();
+            }
+        }
     }
 }
