e2149d07b3eda057dd16ba6739c1202175f27df8
Aleksei Kolechkin <aleksei_kolechkin@epam.com>
2017-12-07 17:57:22 +0300

    Lambda_Part3_Exercise4

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,11 +1,15 @@
 package lambda.part3.exercise;
 
+import com.google.common.collect.Lists;
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
@@ -15,19 +19,38 @@ public class Exercise4 {
 
     private static class LazyFlatMapHelper<T, R> {
 
-        public static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+        private final List<T> source;
+        private final Function<T, List<R>> flatMapping;
+
+        private LazyFlatMapHelper(List<T> source, Function<T, List<R>> flatMapping) {
+            this.source = source;
+            this.flatMapping = flatMapping;
         }
 
-        public <U> LazyFlatMapHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+        public static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
+            return new LazyFlatMapHelper<>(list, Collections::singletonList);
+        }
+
+        public LazyFlatMapHelper<T, R> map(Function<T, R> mapping) {
+            return new LazyFlatMapHelper<>(source, entity ->
+                    applyToList(this.flatMapping.apply(entity),
+                            (element, result) -> result.add(mapping.apply(element))));
+        }
+
+        public LazyFlatMapHelper<T, R> flatMap(Function<T, List<R>> flatMapping) {
+            return new LazyFlatMapHelper<>(source, entity ->
+                    applyToList(this.flatMapping.apply(entity),
+                            (element, result) -> result.addAll(flatMapping.apply(element))));
         }
 
         public List<R> force() {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+            return applyToList(source, (element, result) -> result.addAll(flatMapping.apply(element)));
+        }
+
+        public <U> List<R> applyToList(List<R> source, BiFunction<T, List<R>, Boolean> function){
+            List<R> result = new ArrayList<>();
+            source.forEach(entity -> function.apply(entity, result));
+            return result;
         }
     }
 
@@ -35,17 +58,16 @@ public class Exercise4 {
     public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = Example1.getEmployees();
 
-        List<Integer> codes = null;
-        // TODO                 LazyFlatMapHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .getMapped();
+        List<Integer> codes = LazyFlatMapHelper.from(employees)
+                .flatMap(Employee::getJobHistory)
+                .map(JobHistoryEntry::getPosition)
+                .flatMap(Lists::charactersOf)
+                .map(Integer::valueOf)
+                .force();
         assertEquals(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"), codes);
     }
 
-    private static List<Integer> calcCodes(String...strings) {
+    private static List<Integer> calcCodes(String... strings) {
         List<Integer> codes = new ArrayList<>();
         for (String string : strings) {
             for (char letter : string.toCharArray()) {
