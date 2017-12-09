7bfe0adec37a7ea3142aaf960be47a0d1a45455c
Evgeny Ivanov <FLYABRWAY@gmail.com>
2017-12-09 15:41:02 +0300

    Done task which point is summing elements of array

--- src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
+++ src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
@@ -3,11 +3,49 @@ package spliterator.part1.exercise.forkjoinpool;
 import org.junit.Test;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class FJPSumIntArrayExample {
+
+    private static class Asum extends RecursiveAction {
+        int[] data;
+        int lowBound;
+        int highBound;
+        int sum;
+
+        Asum(int[] data) {
+            this(data, 0, data.length - 1);
+        }
+
+        Asum(int[] data, int lowBound, int highBound) {
+            this.data = data;
+            this.lowBound = lowBound;
+            this.highBound = highBound;
+        }
+
+        @Override
+        protected void compute() {
+            if (lowBound == highBound) {
+                sum = data[lowBound];
+            } else if (lowBound > highBound) {
+                sum = 0;
+            } else {
+                int mid = (lowBound + highBound) >> 1;
+
+                Asum leftSUM = new Asum(data, lowBound, mid);
+                Asum rightSUM = new Asum(data, mid + 1, highBound);
+
+                leftSUM.fork();
+                rightSUM.compute();
+                leftSUM.join();
+
+                sum = leftSUM.sum + rightSUM.sum;
+            }
+        }
+    }
 
     @Test
     public void test() {
@@ -15,8 +53,8 @@ public class FJPSumIntArrayExample {
                               .limit(100)
                               .toArray();
 
-        // TODO реализация
-        int result = new ForkJoinPool().invoke(null);
-        assertEquals(100, result);
+        Asum asum = new Asum(data);
+        new ForkJoinPool().invoke(asum);
+        assertEquals(100, asum.sum);
     }
 }
