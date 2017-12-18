ca495453e4162c82f6a01bc0b76d9171eeb275cb
Leonid Gumeniuk <leonid_gumeniuk@epam.com>
2017-12-18 19:28:49 +0300

    Lambda-Part3_Exercise4_Done

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,10 +1,13 @@
 package lambda.part3.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,19 +18,40 @@ public class Exercise4 {
 
     private static class LazyFlatMapHelper<T, R> {
 
-        public static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+        private final List<T> source;
+        private final Function<T, List<R>> rule;
+
+        private LazyFlatMapHelper(List<T> source, Function<T, List<R>> rule) {
+            this.source = source;
+            this.rule = rule;
         }
 
-        public <U> LazyFlatMapHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+        static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
+            return new LazyFlatMapHelper<>(list, Arrays::asList);
         }
 
-        public List<R> force() {
-            // TODO реализация
-            throw new UnsupportedOperationException();
+        <U> LazyFlatMapHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
+            return new LazyFlatMapHelper<>(source, compose(rule, flatMapping));
+        }
+
+        <U> LazyFlatMapHelper<T, U> map(Function<R, U> mapping) {
+            return new LazyFlatMapHelper<>(source, compose(rule, t -> Collections.singletonList(mapping.apply(t))));
+        }
+
+        private <U> Function<T, List<U>> compose(Function<T, List<R>> function1, Function<R, List<U>> function2) {
+            return list -> {
+                List<U> result = new ArrayList<>();
+                function1.apply(list).forEach(r -> result.addAll(function2.apply(r)));
+                return result;
+            };
+        }
+
+        List<R> force() {
+            List<R> newSource = new ArrayList<>();
+            for (T s : source) {
+                newSource.addAll(rule.apply(s));
+            }
+            return newSource;
         }
     }
 
@@ -35,17 +59,22 @@ public class Exercise4 {
     public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = Example1.getEmployees();
 
-        List<Integer> codes = null;
-        // TODO                 LazyFlatMapHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .getMapped();
+        List<Integer> codes = LazyFlatMapHelper.from(employees)
+                .flatMap(Employee::getJobHistory)
+                .map(JobHistoryEntry::getPosition)
+                .flatMap(string -> {
+                    List<Character> characters = new ArrayList<>();
+                    for (char c : string.toCharArray()) {
+                        characters.add(c);
+                    }
+                    return characters;
+                })
+                .map(c -> (int) c)
+                .force();
         assertEquals(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"), codes);
     }
 
-    private static List<Integer> calcCodes(String...strings) {
+    private static List<Integer> calcCodes(String... strings) {
         List<Integer> codes = new ArrayList<>();
         for (String string : strings) {
             for (char letter : string.toCharArray()) {
