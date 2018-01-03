80b80b31b9b7225f2466c3028fd8fe030fd88074
mihailpoplavkov <mihail.poplavkov@gmail.com>
2018-01-03 15:36:24 +0300

    spliterator.part1.ForkJoinPool

--- src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
+++ src/test/java/spliterator/part1/exercise/forkjoinpool/FJPSumIntArrayExample.java
@@ -3,6 +3,7 @@ package spliterator.part1.exercise.forkjoinpool;
 import org.junit.Test;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
@@ -15,8 +16,38 @@ public class FJPSumIntArrayExample {
                               .limit(100)
                               .toArray();
 
-        // TODO реализация
-        int result = new ForkJoinPool().invoke(null);
+        int result = new ForkJoinPool().invoke(new SumArrayTask(data, 0, data.length - 1));
         assertEquals(100, result);
+    }
+
+    private static class SumArrayTask extends RecursiveTask<Integer> {
+        private static final int SEQUENTIAL_THRESHOLD = 5;
+
+        private int[] data;
+        private int fromInclusive;
+        private int toInclusive;
+
+        SumArrayTask(int[] data, int fromInclusive, int toInclusive) {
+            this.fromInclusive = fromInclusive;
+            this.toInclusive = toInclusive;
+            this.data = data;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (toInclusive - fromInclusive < SEQUENTIAL_THRESHOLD) {
+                int res = 0;
+                for (int i = fromInclusive; i <= toInclusive; i++) {
+                    res += data[i];
+                }
+                return res;
+            } else {
+                int pivot = (fromInclusive + toInclusive) / 2;
+                SumArrayTask left = new SumArrayTask(data, fromInclusive, pivot);
+                SumArrayTask right = new SumArrayTask(data, pivot + 1, toInclusive);
+                left.fork();
+                return right.compute() + left.join();
+            }
+        }
     }
 }
