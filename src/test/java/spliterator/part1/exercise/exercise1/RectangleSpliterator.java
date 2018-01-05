bfc8a4fa7468dccb5d3caa0ace9d887017751e18
mihailpoplavkov <mihail.poplavkov@gmail.com>
2018-01-05 17:12:40 +0300

    spliterator part1 exercise1

--- src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
+++ src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
@@ -1,36 +1,98 @@
 package spliterator.part1.exercise.exercise1;
 
+import lombok.AllArgsConstructor;
+
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
-    public RectangleSpliterator(int[][] array) {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    @AllArgsConstructor
+    private static class Pair {
+        private final int[][] data;
+        int _1;
+        int _2;
+
+        void inc() {
+            _2++;
+            if (_2 == data[_1].length) {
+                _1++;
+                _2 = 0;
+            }
+        }
+
+        boolean moreThan(Pair other) {
+            return _1 > other._1 || _1 == other._1 && _2 > other._2;
+        }
+    }
+
+    private final int[][] data;
+    private final Pair start;
+    private final Pair end;
+
+    private RectangleSpliterator(int[][] data, Pair start, Pair end) {
+        super(calculateSize(data),
+                Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.NONNULL);
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    RectangleSpliterator(int[][] data) {
+        this(data, new Pair(data,0, 0), new Pair(data,data.length - 1, data[data.length - 1].length - 1));
     }
 
     @Override
     public OfInt trySplit() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        long shift = estimateSize() / 2;
+        Pair oldStart = new Pair(data, start._1, start._2);
+        for (int i = 0; i < shift - 1; i++) {
+            start.inc();
+        }
+        OfInt left = new RectangleSpliterator(data, oldStart, start);
+        start.inc();
+        return left;
     }
 
     @Override
     public long estimateSize() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        return calculateSize(data, start, end);
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        if (start.moreThan(end)) {
+            return false;
+        }
+        action.accept(data[start._1][start._2]);
+        start.inc();
+        return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        //noinspection StatementWithEmptyBody
+        do {} while (tryAdvance(action));
     }
+
+    private static long calculateSize(int[][] data, Pair start, Pair end) {
+        long size = 0;
+        if (start._1 < end._1) {
+            size += data[start._1].length - start._2;
+            for (int i = start._1 + 1; i < end._1; i++) {
+                size += data[i].length;
+            }
+            size += end._2 + 1;
+        } else {
+            size += end._2 - start._2 + 1;
+        }
+        return size;
+    }
+
+    private static long calculateSize(int[][] data) {
+        return calculateSize(data, new Pair(data,0, 0),
+                new Pair(data,data.length - 1, data[data.length - 1].length - 1));
+    }
+
 }
