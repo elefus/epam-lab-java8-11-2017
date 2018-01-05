32070bb5cb03a0b1828106579414735cc22d051d
super64812 <iserdz812@gmail.com>
2018-01-05 17:25:32 +0300

    streams.part2.exercise1

--- src/test/java/streams/part2/exercise/Exercise1.java
+++ src/test/java/streams/part2/exercise/Exercise1.java
@@ -1,11 +1,14 @@
 package streams.part2.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
+import streams.part2.example.data.PersonPositionPair;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,7 +20,16 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        Long hours = null;
+        Long hours = employees
+                .stream()
+                .flatMapToLong(employee -> employee
+                        .getJobHistory()
+                        .stream()
+                        .filter(jobHistoryEntry -> jobHistoryEntry
+                                .getEmployer()
+                                .equals("EPAM"))
+                        .mapToLong(JobHistoryEntry::getDuration))
+                .sum();
 
         assertEquals(18, hours.longValue());
     }
@@ -27,7 +39,15 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        Set<Person> workedAsQa = null;
+        Set<Person> workedAsQa = employees
+                .stream()
+                .filter(employee -> employee
+                        .getJobHistory()
+                        .stream()
+                        .map(JobHistoryEntry::getPosition)
+                        .anyMatch("QA"::equals))
+                .map(Employee::getPerson)
+                .collect(Collectors.toSet());
 
         Set<Person> expected = new HashSet<>(Arrays.asList(
                 employees.get(2).getPerson(),
@@ -42,7 +62,12 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        String result = null;
+        String result = employees
+                .stream()
+                .map(employee -> employee
+                        .getPerson()
+                        .getFullName())
+                .collect(Collectors.joining("\n"));
 
         String expected = "Иван Мельников\n"
                         + "Александр Дементьев\n"
@@ -58,7 +83,20 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees
+                .stream()
+                .map(employee -> new PersonPositionPair(employee
+                        .getPerson(),
+                        employee
+                        .getJobHistory()
+                        .get(0)
+                        .getPosition()))
+                .collect(Collectors
+                        .toMap(PersonPositionPair::getPosition,
+                                pair -> new HashSet<>(Collections
+                                        .singleton(pair.getPerson())),
+                                (set1,set2) -> {set2.addAll(set1);
+                                return set2;}));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("dev", Collections.singleton(employees.get(0).getPerson()));
@@ -76,7 +114,15 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees
+                .stream()
+                .map(employee -> new PersonPositionPair(employee.getPerson(),
+                        employee
+                        .getJobHistory()
+                                .get(0)
+                                .getPosition()))
+                .collect(Collectors
+                        .groupingBy(PersonPositionPair::getPosition, Collectors.mapping(PersonPositionPair::getPerson, Collectors.toSet())));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("dev", Collections.singleton(employees.get(0).getPerson()));
