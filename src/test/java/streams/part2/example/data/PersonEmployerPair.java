b3886698cea3887ed04270fa4ffb8cb05d40b3d9
Leonid Gumeniuk <leonid_gumeniuk@epam.com>
2018-01-09 01:54:22 +0300

    Streams-Part2_Exercise2_Optimization

--- src/test/java/streams/part2/example/data/PersonEmployerPair.java
+++ src/test/java/streams/part2/example/data/PersonEmployerPair.java
@@ -1,22 +1,12 @@
 package streams.part2.example.data;
 
 import lambda.data.Person;
+import lombok.Data;
 
+@Data
 public class PersonEmployerPair {
+
     private final Person person;
     private final String employer;
-
-    public PersonEmployerPair(Person person, String employer) {
-        this.person = person;
-        this.employer = employer;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public String getEmployer() {
-        return employer;
-    }
 }
 
