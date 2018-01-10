ce814f054a69bae39e1ddad90f13d7caee7783d8
MihailPoplavkov <mihail.poplavkov@gmail.com>
2018-01-10 19:21:28 +0300

    Fix spliterator p1 ex1

--- src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
+++ src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
@@ -9,29 +9,34 @@ import java.util.function.IntConsumer;
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
     @AllArgsConstructor
-    private static class Pair {
+    private static class MatrixPointer {
         private final int[][] data;
-        int _1;
-        int _2;
+        int row;
+        int col;
 
-        void inc() {
-            _2++;
-            if (_2 == data[_1].length) {
-                _1++;
-                _2 = 0;
+        void inc(int inc) {
+            col += inc;
+            int rowLength;
+            while (row < data.length && col >= (rowLength = data[row].length)) {
+                col -= rowLength;
+                row++;
             }
         }
 
-        boolean moreThan(Pair other) {
-            return _1 > other._1 || _1 == other._1 && _2 > other._2;
+        void inc() {
+            inc(1);
+        }
+
+        boolean moreThan(MatrixPointer other) {
+            return row > other.row || row == other.row && col > other.col;
         }
     }
 
     private final int[][] data;
-    private final Pair start;
-    private final Pair end;
+    private final MatrixPointer start;
+    private final MatrixPointer end;
 
-    private RectangleSpliterator(int[][] data, Pair start, Pair end) {
+    private RectangleSpliterator(int[][] data, MatrixPointer start, MatrixPointer end) {
         super(calculateSize(data),
                 Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.NONNULL);
         this.data = data;
@@ -40,16 +45,14 @@ public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
     }
 
     RectangleSpliterator(int[][] data) {
-        this(data, new Pair(data,0, 0), new Pair(data,data.length - 1, data[data.length - 1].length - 1));
+        this(data, new MatrixPointer(data, 0, 0), new MatrixPointer(data, data.length - 1, data[data.length - 1].length - 1));
     }
 
     @Override
     public OfInt trySplit() {
         long shift = estimateSize() / 2;
-        Pair oldStart = new Pair(data, start._1, start._2);
-        for (int i = 0; i < shift - 1; i++) {
-            start.inc();
-        }
+        MatrixPointer oldStart = new MatrixPointer(data, start.row, start.col);
+        start.inc((int) shift - 1);
         OfInt left = new RectangleSpliterator(data, oldStart, start);
         start.inc();
         return left;
@@ -65,34 +68,36 @@ public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
         if (start.moreThan(end)) {
             return false;
         }
-        action.accept(data[start._1][start._2]);
+        action.accept(data[start.row][start.col]);
         start.inc();
         return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        //noinspection StatementWithEmptyBody
-        do {} while (tryAdvance(action));
+        while (!start.moreThan(end)) {
+            action.accept(data[start.row][start.col]);
+            start.inc();
+        }
     }
 
-    private static long calculateSize(int[][] data, Pair start, Pair end) {
+    private static long calculateSize(int[][] data, MatrixPointer start, MatrixPointer end) {
         long size = 0;
-        if (start._1 < end._1) {
-            size += data[start._1].length - start._2;
-            for (int i = start._1 + 1; i < end._1; i++) {
+        if (start.row < end.row) {
+            size += data[start.row].length - start.col;
+            for (int i = start.row + 1; i < end.row; i++) {
                 size += data[i].length;
             }
-            size += end._2 + 1;
+            size += end.col + 1;
         } else {
-            size += end._2 - start._2 + 1;
+            size += end.col - start.col + 1;
         }
         return size;
     }
 
     private static long calculateSize(int[][] data) {
-        return calculateSize(data, new Pair(data,0, 0),
-                new Pair(data,data.length - 1, data[data.length - 1].length - 1));
+        return calculateSize(data, new MatrixPointer(data, 0, 0),
+                new MatrixPointer(data, data.length - 1, data[data.length - 1].length - 1));
     }
 
 }
