06d541f62f5331832f551687af34a2ab6510ca3a
super64812 <iserdz812@gmail.com>
2018-01-13 16:54:47 +0300

    spliterator.part1.rectangleSpliterator

--- src/test/java/spliterator/part1/exercise/exercise1/Exercise1.java
+++ src/test/java/spliterator/part1/exercise/exercise1/Exercise1.java
@@ -13,10 +13,10 @@ public class Exercise1 {
     @Test
     public void testSumUsingSpliterator() {
         int[][] data = Stream.generate(() -> IntStream.generate(() -> 1)
-                                                      .limit(5)
-                                                      .toArray())
-                             .limit(10)
-                             .toArray(int[][]::new);
+                .limit(5)
+                .toArray())
+                .limit(10)
+                .toArray(int[][]::new);
 
         IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
 
@@ -26,10 +26,10 @@ public class Exercise1 {
     @Test
     public void testMinUsingSpliterator() {
         int[][] data = Stream.generate(() -> IntStream.generate(() -> 42)
-                                                      .limit(5)
-                                                      .toArray())
-                             .limit(10)
-                             .toArray(int[][]::new);
+                .limit(5)
+                .toArray())
+                .limit(10)
+                .toArray(int[][]::new);
         data[0][0] = 0;
 
         IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
@@ -40,10 +40,10 @@ public class Exercise1 {
     @Test
     public void testMaxUsingSpliterator() {
         int[][] data = Stream.generate(() -> IntStream.generate(() -> 42)
-                                                      .limit(5)
-                                                      .toArray())
-                             .limit(10)
-                             .toArray(int[][]::new);
+                .limit(5)
+                .toArray())
+                .limit(10)
+                .toArray(int[][]::new);
         data[3][3] = 50;
 
         IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
