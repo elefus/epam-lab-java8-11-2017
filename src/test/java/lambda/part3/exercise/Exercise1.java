1b3c614487d88521f733e3f5233a769768e49835
mikhail <mikhail_sosnin@epam.com>
2018-01-14 18:12:41 +0300

    lambda.part3.exercise1(Sosnin)

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -4,8 +4,10 @@ import lambda.data.Employee;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,13 +17,13 @@ public class Exercise1 {
     @Test
     public void mapEmployeesToLengthOfTheirFullNames() {
         List<Employee> employees = Example1.getEmployees();
-        List<Integer> lengths = null;
+        List<Integer> lengths = new ArrayList<>();
 
-        // TODO функция извлечения полного имени из сотрудника fullNameExtractor: Employee -> String
-        // TODO функция извлечения длины из строки stringLengthExtractor: String -> Integer
-        // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
-        // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
-
+        Function<Employee, String> fullNameExtractor = employee -> employee.getPerson().getFullName();
+        Function<String, Integer> stringLengthExtractor = String::length;
+        Function<Employee, Integer> fullNameLengthExtractor = employee ->
+                stringLengthExtractor.apply(fullNameExtractor.apply(employee));
+        employees.forEach(employee -> lengths.add(fullNameLengthExtractor.apply(employee)));
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
     }
 }
