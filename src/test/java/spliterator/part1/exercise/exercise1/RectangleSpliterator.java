32884e56b5664113493ea1a94a2742a8013bfc7c
Aleksei Kolechkin <aleksei_kolechkin@epam.com>
2018-01-15 00:36:18 +0300

    Spliterator_Part1_RectangleSpliterator

--- src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
+++ src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
@@ -1,36 +1,99 @@
 package spliterator.part1.exercise.exercise1;
 
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
+    private static class Position {
+
+        private int row;
+        private int column;
+        private final int rowLength;
+
+        Position(Position another) {
+            row = another.row;
+            column = another.column;
+            rowLength = another.rowLength;
+        }
+
+        Position(int row, int column, int rowLength) {
+            this.row = row;
+            this.column = column;
+            this.rowLength = rowLength;
+        }
+
+        void move(int positions) {
+            column += positions;
+            row += column / rowLength;
+            column %= rowLength;
+        }
+
+        void move() {
+            move(1);
+        }
+
+        int movesTo(Position another) {
+            return (another.row - row) * rowLength + another.column - column + 1;
+        }
+    }
+
+    private final int[][] data;
+    private Position first;
+    private Position last;
+
     public RectangleSpliterator(int[][] array) {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+        this(
+                array,
+                new Position(0, 0, array[0].length),
+                new Position(array.length - 1, array[array.length - 1].length - 1,array[0].length)
+        );
+    }
+
+    private RectangleSpliterator(int[][] data, Position current, Position last) {
+        super(
+                current.movesTo(last),
+        Spliterator.IMMUTABLE |
+                Spliterator.ORDERED |
+                Spliterator.SIZED |
+                Spliterator.NONNULL
+        );
+        this.first = current;
+        this.last = last;
+        this.data = data;
     }
 
     @Override
     public OfInt trySplit() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        Position mid = new Position(first);
+        mid.move(first.movesTo(last)/2);
+        RectangleSpliterator half = new RectangleSpliterator(data, first, mid);
+        first = mid;
+        return half;
     }
 
     @Override
     public long estimateSize() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        return first.movesTo(last);
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        if (first.row == last.row && first.column == last.column) {
+            return false;
+        }
+        action.accept(data[first.row][first.column]);
+        first.move();
+        return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        int movesToLast = first.movesTo(last);
+        for (int i = 0; i < movesToLast; ++i) {
+            action.accept(data[first.row][first.column]);
+            first.move();
+        }
     }
 }
