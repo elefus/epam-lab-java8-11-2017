f0077c8bbadb9ed6899103472f72e77afdd502e4
Natalia <natalia_leznikova@hotmail.com>
2018-01-14 20:05:25 +0300

    streams part 1 exercise 2 done

--- src/test/java/streams/part1/exercise/Exercise2.java
+++ src/test/java/streams/part1/exercise/Exercise2.java
@@ -31,7 +31,7 @@ public class Exercise2 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO реализация
+        candidates.replaceAll( (person, status) -> person.getAge() > 21 ? Status.ACCEPTED : Status.DECLINED);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -48,7 +48,7 @@ public class Exercise2 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO реализация
+        candidates.replaceAll( (person, status) -> person.getAge() > 21 ? Status.ACCEPTED : null);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -64,10 +64,9 @@ public class Exercise2 {
         candidates.put(alex, Status.PENDING);
         candidates.put(ivan, Status.PENDING);
 
-        // TODO реализация
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Status alexStatus = candidates.getOrDefault(alex, Status.UNKNOWN);
+        Status ivanStatus = candidates.getOrDefault(ivan, Status.UNKNOWN);
+        Status helenStatus = candidates.getOrDefault(helen, Status.UNKNOWN);
 
         assertEquals(Status.PENDING, alexStatus);
         assertEquals(Status.PENDING, ivanStatus);
@@ -87,7 +86,7 @@ public class Exercise2 {
         newValues.put(alex, Status.DECLINED);
         newValues.put(helen, Status.PENDING);
 
-        // TODO реализация
+        oldValues.forEach( (person, status) -> newValues.merge(person, status, (oldStatus, newStatus) -> oldStatus) );
 
         assertEquals(Status.DECLINED, newValues.get(alex));
         assertEquals(Status.ACCEPTED, newValues.get(ivan));
