6ded5267a3c6b80bf191c7113f6deec1c29bf91d
Alexander Radchenko <partner.ar@gmail.com>
2018-01-15 03:54:24 +0300

    Spliterator.part1.example1 - RectangleSpliterator

--- src/test/java/spliterator/part1/exercise/exercise1/Exercise1.java
+++ src/test/java/spliterator/part1/exercise/exercise1/Exercise1.java
@@ -2,6 +2,8 @@ package spliterator.part1.exercise.exercise1;
 
 import org.junit.Test;
 
+import java.util.Spliterator;
+import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -49,5 +51,64 @@ public class Exercise1 {
         IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
 
         assertEquals(50, stream.max().orElseThrow(IllegalStateException::new));
+    }
+
+    @Test
+    public void testEstimateSize50() {
+        int[][] data = Stream.generate(() -> IntStream.generate(() -> 1)
+                                                      .limit(5)
+                                                      .toArray())
+                             .limit(10)
+                             .toArray(int[][]::new);
+
+        IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
+
+        assertEquals(50, stream.spliterator().estimateSize());
+    }
+
+    @Test
+    public void testTrySplit() {
+        int[][] data = Stream.generate(() -> IntStream.generate(() -> 1)
+                                                      .limit(5)
+                                                      .toArray())
+                             .limit(10)
+                             .toArray(int[][]::new);
+
+        IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
+        Spliterator.OfInt spl = stream.spliterator();
+        assertEquals(50, spl.estimateSize());
+
+        Spliterator.OfInt splittedOnce = spl.trySplit();
+        assertEquals(25, splittedOnce.estimateSize());
+        assertEquals(25, spl.estimateSize());
+
+        Spliterator.OfInt splittedTwice = splittedOnce.trySplit();
+        assertEquals(12, splittedTwice.estimateSize());
+        assertEquals(13, splittedOnce.estimateSize());
+
+        Spliterator.OfInt splittedOnceAgain = splittedOnce.trySplit();
+        assertEquals(6, splittedOnceAgain.estimateSize());
+        assertEquals(7, splittedOnce.estimateSize());
+
+        Spliterator.OfInt splittedTwiceAgain = splittedTwice.trySplit();
+        assertEquals(6, splittedTwiceAgain.estimateSize());
+        assertEquals(6, splittedTwice.estimateSize());
+    }
+
+    @Test
+    public void testForEachRemaining() {
+        int[][] data = Stream.generate(() -> IntStream.generate(() -> 1)
+                                                      .limit(5)
+                                                      .toArray())
+                             .limit(10)
+                             .toArray(int[][]::new);
+        data[3][3] = 50;
+
+        IntStream stream = StreamSupport.intStream(new RectangleSpliterator(data), false);
+        Spliterator.OfInt spl = stream.spliterator();
+        spl.tryAdvance((IntConsumer) i -> System.out.println("TryAdvance 1 i = " + i));
+        spl.tryAdvance((IntConsumer) i -> System.out.println("TryAdvance 2 i = " + i));
+        spl.forEachRemaining((IntConsumer) System.out::println);
+        System.out.println("Est size = " + spl.estimateSize());
     }
 }
--- src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
+++ src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
@@ -1,36 +1,107 @@
 package spliterator.part1.exercise.exercise1;
 
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
-    public RectangleSpliterator(int[][] array) {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    private final int[][] source;
+    private Index currentIndex;
+    private Index lastIndex;
+
+    private static class Index implements Cloneable {
+        private int[][] source;
+        private int x;
+        private int y;
+        private boolean canIncrement;
+        int rowLength;
+        int colLength;
+
+        Index(int[][] array, int x, int y) {
+            this.source = array;
+            this.x = x;
+            this.y = y;
+            colLength = source.length;
+            rowLength = source[0].length;
+            canIncrement = x < rowLength && y < colLength;
+        }
+
+        boolean canIncrement() {
+            return canIncrement;
+        }
+
+        void increment() {
+            if (x == rowLength - 1) {
+                x = 0;
+                y++;
+            } else {
+                x++;
+            }
+            if (y == colLength) {
+                canIncrement = false;
+            }
+        }
+
+        Index median(Index lastIndex) {
+            int offset = (lastIndex.diff(this) + 1) / 2;
+            int newX = (x + offset - 1) % rowLength;
+            int newY = y + (x + offset - 1) / rowLength;
+            return new Index(source, newX, newY);
+        }
+
+        int diff(Index fromIndex) {
+            return x - fromIndex.x + rowLength * (y - fromIndex.y);
+        }
+
+        @Override
+        @SneakyThrows
+        final protected Object clone() {
+            return super.clone();
+        }
+    }
+
+    public RectangleSpliterator(@NonNull int[][] array) {
+        this(array, new Index(array, 0, 0), new Index(array, array[0].length - 1, array.length - 1));
+    }
+
+    public RectangleSpliterator(@NonNull int[][] array, Index from, Index to) {
+        super(to.diff(from) + 1, IMMUTABLE|NONNULL|SIZED|SUBSIZED);
+        this.source = array;
+        this.currentIndex = (Index) from.clone();
+        this.lastIndex = (Index) to.clone();
     }
 
     @Override
     public OfInt trySplit() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        Index medianIndex = currentIndex.median(lastIndex);
+        RectangleSpliterator newSpliterator = new RectangleSpliterator(source, currentIndex, medianIndex);
+        currentIndex = (Index) medianIndex.clone();
+        currentIndex.increment();
+        return newSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        return lastIndex.diff(currentIndex) + 1;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        if (currentIndex.canIncrement()) {
+            action.accept(source[currentIndex.y][currentIndex.x]);
+            currentIndex.increment();
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    @Override
-    public void forEachRemaining(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
-    }
+//    @Override
+//    public void forEachRemaining(IntConsumer action) {
+//        // TODO реализация
+//        throw new UnsupportedOperationException();
+//    }
 }
