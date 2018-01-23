072c6f5b92ebc2b2454f3b391942401324e254d2
Veniamin_Bakhar <Veniamin_Bakhar@epam.com>
2018-01-23 13:19:15 +0300

    Streams part1 ex.2 done

--- src/test/java/streams/part1/exercise/Exercise2.java
+++ src/test/java/streams/part1/exercise/Exercise2.java
@@ -1,11 +1,10 @@
 package streams.part1.exercise;
 
-import lambda.data.Employee;
 import lambda.data.Person;
-import lambda.part3.example.Example1;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -31,7 +30,7 @@ public class Exercise2 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO реализация
+        candidates.replaceAll( (person, status) -> person.getAge() > 21 ? Status.ACCEPTED : Status.DECLINED);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -48,7 +47,7 @@ public class Exercise2 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO реализация
+        candidates.replaceAll( (person, status) -> person.getAge() > 21 ? Status.ACCEPTED : null);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -65,9 +64,10 @@ public class Exercise2 {
         candidates.put(ivan, Status.PENDING);
 
         // TODO реализация
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Status alexStatus = candidates.getOrDefault(alex, Status.UNKNOWN);
+        Status ivanStatus = candidates.getOrDefault(ivan, Status.UNKNOWN);
+        Status helenStatus = candidates.getOrDefault(helen, Status.UNKNOWN);
+
 
         assertEquals(Status.PENDING, alexStatus);
         assertEquals(Status.PENDING, ivanStatus);
@@ -87,7 +87,7 @@ public class Exercise2 {
         newValues.put(alex, Status.DECLINED);
         newValues.put(helen, Status.PENDING);
 
-        // TODO реализация
+        oldValues.forEach( (person, status) -> newValues.merge(person, status, (oldStatus, newStatus) -> oldStatus) );
 
         assertEquals(Status.DECLINED, newValues.get(alex));
         assertEquals(Status.ACCEPTED, newValues.get(ivan));
